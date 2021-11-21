ae59e3007b05b1be248c5ad9e6af761435471ce1
Mike Potvin <mike@redbitdev.com>
2021-11-21 18:24:46 -0500

    removed comment

--- utilities/ApiUtil.tsx
+++ utilities/ApiUtil.tsx
@@ -1,7 +1,3 @@
-/* 
-  When a view needs to make a HTTP call it should go through a Service ( such as LoginService.js )
-  The service classes will use one of the responses in this util to ensure it's handling data & errors consistantly.
-*/
 interface ApiErrorResponse {
   code: string;
   data: any;
